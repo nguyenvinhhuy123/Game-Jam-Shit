ff1fdce43b77cce90c3055d4ecab4f2bf1d3994d
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2023-10-10 21:46:29 +0700

    reimport asset

---
+++ Assets/Scenes/Card Game/Card Component/Card.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum CardType
+{
+    Monster,
+    Spell,
+    Trap,
+
+}
+public class Card : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/Card Game/Card Component/CardSOData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Game-Jam-Shit/CardSOData")]
+
+public class CardSOData : ScriptableObject
+{
+    
+}
