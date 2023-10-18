e3fdd44de6f24c6e14be8595d45aac825762d472
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2023-10-18 21:13:27 +0700

    test: testing UseAttack and UseSpell Callback

--- Assets/Scenes/Card Game/Script/Manager/DebbugerManager.cs
+++ Assets/Scenes/Card Game/Script/Manager/DebbugerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class DebbugerManager : MonoBehaviour
 {
@@ -11,8 +12,13 @@ public class DebbugerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //!for testing with health and attack communication
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            int healthMonster2Before = Monster2.m_component.m_health.CurrentHealthValue;
+            Monster1.UseNormalAttack(Monster2);
+            Assert.AreNotEqual(Monster2.m_component.m_health.CurrentHealthValue, healthMonster2Before);
         }
+        
     }
 }
