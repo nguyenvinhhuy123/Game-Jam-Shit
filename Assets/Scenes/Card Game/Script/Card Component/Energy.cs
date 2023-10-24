d2fab2f22d3b0f0dc099329baad59f3b246fef8b
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2023-10-24 20:22:30 +0700

    feat: Add UnityAction and Action callback for component

--- Assets/Scenes/Card Game/Script/Card Component/Energy.cs
+++ Assets/Scenes/Card Game/Script/Card Component/Energy.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Energy
+public class Energy : MonoBehaviour
 {
     [SerializeField] private MonsterType m_energyType;
     public MonsterType Type {get {return m_energyType;}}
     [SerializeField] private bool m_used;
     public bool Used {get {return m_used;}}
+    public void OnHover()
+    {
 
+    }
+    public void OnSelect()
+    {
+        
+    }
     public void UseThis()
     {
         if (m_used)
