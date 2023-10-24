4bd3e6ad4ca7ab77f8ff4f85933ef12f0b100bcc
Nguyễn Việt Kim <kimnguyenblc@gmail.com>
2023-10-24 20:17:49 +0700

    Add card UI asset

---
+++ Assets/Card Assets/Scripts/Card.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Card : using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int id;
+    public int health;
+    public string cardName;
+    public int normalAtk;
+}
