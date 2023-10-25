37e488f2a367a5b2b59d333bc4859c2a3d1a8216
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2023-10-25 21:53:12 +0700

    feat: add Add Card functionality for later develop

--- Assets/Scenes/Card Game/Script/DeckBuilder/BuildedDeckContainer.cs
+++ Assets/Scenes/Card Game/Script/DeckBuilder/BuildedDeckContainer.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
-
 [CreateAssetMenu(fileName = "BuildedDeckContainer", menuName = "Game-Jam-Shit/BuildedDeckContainer", order = 0)]
 public class BuildedDeckContainer : ScriptableObject {
     public MonsterCard[] MonsterCards = new MonsterCard[3];
     public SpellCard[] SpellCards = new SpellCard[20];
     public Energy[] Energies = new Energy[10];
+    public void AddMonster()
+    {
+        
+    }
+    public void AddSpell()
+    {
+
+    }
+    public void AddEnergy()
+    {
+
+    }
 }
