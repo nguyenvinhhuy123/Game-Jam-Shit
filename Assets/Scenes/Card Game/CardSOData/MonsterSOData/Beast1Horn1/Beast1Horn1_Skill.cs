2ecdf127f0c5d4de810197a15875546ad07c6d21
Athena <bqh1402003@gmail.com>
2023-10-27 17:59:04 +0700

    update buff end condition

--- Assets/Scenes/Card Game/CardSOData/MonsterSOData/Beast1Horn1/Beast1Horn1_Skill.cs
+++ Assets/Scenes/Card Game/CardSOData/MonsterSOData/Beast1Horn1/Beast1Horn1_Skill.cs
@@ -8,14 +8,12 @@ using BuffSystem;
 [CreateAssetMenu(menuName = "MonsterAbility/Beast1Horn1/Skill")]
 public class Beast1Horn1_Skill : MonsterSkill
 {
-
     private NABuffBaseSOData m_NABuffSOData;
     private NABuffHandler m_NABUffHandler;
     public int m_NABuffAmount = 3;
     public int m_duration = 1;
     //check for end on external call (when the monster use Normal Attack again after getting the buff)
-    private LifeTimeType m_lifeTimeType = LifeTimeType.END_ON_EXTERNAL_CALL;
-    // private UnityAction<PlayerAuthority> OnTurnChangeAction;
+    private LifeTimeType m_lifeTimeType = LifeTimeType.END_ON_TIME;
     public Beast1Horn1_Skill()
     {
         SkillDescription = "Deal 1 Damage, The next Normal Attack will deal an additional of 3 damage";
@@ -26,7 +24,7 @@ public class Beast1Horn1_Skill : MonsterSkill
         if (m_NABUffHandler == null)
         {
             m_NABuffSOData = NABuffBaseSOData.CreateInstance<NABuffBaseSOData>();
-            m_NABUffHandler = m_NABuffSOData.InitHandler(target, caller) as NABuffHandler;
+            m_NABUffHandler = m_NABuffSOData.InitHandler(target, user.gameObject) as NABuffHandler;
             m_NABuffSOData.Duration = m_duration;
             m_NABuffSOData.EndConditionType = m_lifeTimeType;
             m_NABuffSOData.NABuffAmount = m_NABuffAmount;
