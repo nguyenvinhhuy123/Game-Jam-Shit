4022e0c3c23226a1412365b4e9a33a405e81c3dd
Athena <bqh1402003@gmail.com>
2023-10-28 11:56:11 +0700

    Update synergy to function

--- Assets/Scenes/Card Game/Script/TraitSynergy/synergy.cs
+++ Assets/Scenes/Card Game/Script/TraitSynergy/synergy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using BuffSystem;
 using Game;
+using System;
 
 public class TraitSynergy : MonoBehaviour
 {
@@ -69,127 +70,136 @@ public class TraitSynergy : MonoBehaviour
                     }
                     break;
 
-
-                case m_trait[Aquatic] == 3:
-                //TODO: reduce energy cost 
-
                 // 2 Reptile: All Reptile monsters gain 3 additional max HP.
-                case m_trait[Reptile] == 2:
-                    foreach (MonsterTrait trait in MonsterCards)
+                // 3 Reptile: Once per game, revive the first Reptile monster that gets taken down back up 5 HP.  
+                case MonsterTrait.Reptile:
+                    if (entry.Value == 2)
                     {
-                        if (trait == "Reptile")
-                        {
-                            MonsterCard[trait].Health += 3;
-                        }
+                        //TODO
                     }
-
-                // 3 Reptile: Once per game, revive the first Reptile monster that gets taken down back up 5 HP.    
-                case m_trait[Reptile] == 3:
-                //todo: revive :trolldespair:
-
+                    else if (entry.Value == 3)
+                    {
+                        //TODD: revive :trolldespair:
+                    }
+                    break;
                 // 2 Bug: Every time a Bug uses a Skill to attack an opponent monster,
                 // that monster will get poisoned (-1 HP per turn) for 1 turn.
-                case m_trait[Bug] == 2:
-                    if (MonsterCard.trait == "Bug" && MonsterSkill.OnUse(target, Bug, player))
-                    {
-                        target.ApplyDOTSelf(1, 1)
-                                            }
-
                 // 3 Bug: Bug's poison now last 1 additional turn
-                case m_trait[Bug] == 3:
-                    if (MonsterCard.trait == "Bug" && MonsterSkill.OnUse(target, Bug, player))
+                case MonsterTrait.Bug:
+                    if (entry.Value == 2)
                     {
-                        target.ApplyDOTSelf(1, 1)
-                                            }
+                        //TODO
+                    }
+                    else if (entry.Value == 3)
+                    {
+                        //TODD: 
+                    }
+                    break;
 
                 // 2 Plant: Every time a Plant monster uses a Skill, heals that monster for 1 HP.
-                case m_trait[Plant] == 2:
-                    if (MonsterCard.trait == "Plant" && MonsterSkill.OnUse(target, Plant, player))
-                    {
-                        target.HealthSelf(1)
-                                            }
-
                 // 3 Plant: Heal the active Plant monster for 1 HP per turn.
-                case m_trait[Plant] == 3:
-                    OnEndOfTurn.AddListener(HealthSelf(1))
-
-            // +1 Bird Energy per turn, Aquatic Skill damage deals 1 less damage.
-            case m_trait[Aquatic] >= 1 && m_trait[Bird] >= 1:
-                    Energy.AddEnergy(Bird, 1);
-                //TODO: Aquatic Skill deal 1 less damage
-
-                // Draw a card every time Aquatic monster use a SKill, Bug monster Skill deals 1 less damage
-                case m_trait[Aquatic] >= 1 && m_trait[Bug] >= 1:
-                    if (MonsterCard.traits == "Aquatic" && MonsterSkill.OnUse(target, Plant, player))
+                case MonsterTrait.Plant:
+                    if (entry.Value == 2)
                     {
-                        DrawCard();
+                        //TODO
                     }
-                    //TODO: Bug Skill deal 1 less damage
+                    else if (entry.Value == 3)
+                    {
+                        //TODD: 
+                    }
+                    break;
             }
+        }
+    }
 
-            // Gain 1 additional Plant Energy when game start, Aquatic Skill damage deals 1 less damage.
-            case m_trait[Aquatic] >= 1 && m_trait[Plant] >= 1:
-            //TODO: on game start gain 1 more plant energy
-            //TODO: Aquatic Skill deal 1 less damage
+    // If this trait synergy is available, create a gameObject contain this synergy behavior in scene.
+    // This will work throughout the game scene until game end. 
+    public void checkSingleTraitSynergy()
+    {
+        foreach (KeyValuePair<MonsterTrait, int> entry in m_trait)
+        {
+            if (entry.Key == MonsterTrait.Aquatic && m_trait.ContainsKey(MonsterTrait.Bird))
+            {
+                GameManager.Instance.gameObject.Instantiate("AquaticAndBird");
+                // +1 Bird Energy per turn, Aquatic Skill damage deals 1 less damage.
+                //Energy.AddEnergy(Bird, 1);
 
-            // Once A Turn Aquatic monster heal 1 Health , Reptile Skill costs 1 more Reptile Energy.
-            case m_trait[Aquatic] >= 1 && m_trait[Reptile] >= 1:
+                //TODO: Aquatic Skill deal 1 less damage
+            }
+            else if (entry.Key == MonsterTrait.Aquatic && m_trait.ContainsKey(MonsterTrait.Bug))
+            {
+                // Draw a card every time Aquatic monster use a SKill, Bug monster Skill deals 1 less damage
+                if (MonsterCard.trait == "Aquatic" && MonsterSkill.OnUse(target, Plant, player))
+                {
+                    //DrawCard();
+                }
+                //TODO: Bug Skill deal 1 less damage
+            }
+            else if (entry.Key == MonsterTrait.Aquatic && m_trait.ContainsKey(MonsterTrait.Plant))
+            {
+                // Gain 1 additional Plant Energy when game start, Aquatic Skill damage deals 1 less damage.
+                //TODO: on game start gain 1 more plant energy
+                //TODO: Aquatic Skill deal 1 less damage
+            }
+            else if (entry.Key == MonsterTrait.Aquatic && m_trait.ContainsKey(MonsterTrait.Reptile))
+            {
+                // Once A Turn Aquatic monster heal 1 Health , Reptile Skill costs 1 more Reptile Energy.
                 if (MonsterCard.traits == "Reptile")
                 {
-                    OnEndOfTurn.AddListener(HealthSelf(1))
-                                                    }
-            //TODO: Reptile Skill cost 1 more Reptile energy
-
-            // Once per turn Draw a card when the first Bird use a Normal attack, -2 max HP for Bug.
-            case m_trait[Bird] >= 1 && m_trait[Bug] >= 1:
+                    //OnEndOfTurn.AddListener(HealthSelf(1));
+                    //TODO: Reptile Skill cost 1 more Reptile energy
+                }
+            }
+            else if (entry.Key == MonsterTrait.Bird && m_trait.ContainsKey(MonsterTrait.Bug))
+            {
+                // Once per turn Draw a card when the first Bird use a Normal attack, -2 max HP for Bug.
                 if (MonsterCard.traits == "Bird" && MonsterNormalAttack.OnUse(target, Plant, player))
                 {
                     OnEndOfTurn.addListener(DrawCard());
                 }
-                foreach (Monster
-                                                    if (MonsterCard.traits == "Bug")
-                    {
-                        MonsterCard.Health -= 2
-                }
-
-            //Gain 1 additional Plant Energy when game start, Bird Normal Attack needs 1 more other Energy to use.
-            case m_trait[Bird] >= 1 && m_trait[Plant] > 1:
-            // TODO: on game start gain 1 more plant energy
-            // TODO: Bird Normal Attack needs 1 more other Energy to use.
-
-            // Reptile Skill cost 1 less other Energy, Bird -2 max HP
-            case m_trait[Bird] >= 1 && m_trait[Reptile]:
-                // TODO: Reptile skill cost 1 less other energy
-                if (MonsterCard.traits == "Bug")
+                foreach ()
                 {
-                    MonsterCard.Health -= 2
-                                                    }
-
-            // Every time Bug uses a Skill Attack, heals 1 HP; Plant Skill damage deals 1 less damage.
-            case m_trait[Bug] >= 1 && m_trait[Plant] >= 1:
+                    MonsterCard.Health -= 2;
+                }
+            }
+            else if (entry.Key == MonsterTrait.Bird && m_trait.ContainsKey(MonsterTrait.Plant))
+            {
+                //Gain 1 additional Plant Energy when game start, Bird Normal Attack needs 1 more other Energy to use.
+                // TODO: on game start gain 1 more plant energy
+                // TODO: Bird Normal Attack needs 1 more other Energy to use.
+            }
+            else if (entry.Key == MonsterTrait.Bird && m_trait.ContainsKey(MonsterTrait.Reptile))
+            {
+                // Reptile Skill cost 1 less other Energy, Bird -2 max HP
+                // TODO: 
+            }
+            else if (entry.Key == MonsterTrait.Bug && m_trait.ContainsKey(MonsterTrait.Plant))
+            {
+                // Every time Bug uses a Skill Attack, heals 1 HP; Plant Skill damage deals 1 less damage.
                 if (MonsterCard.traits == "Bug" && MonsterNormalAttack.OnUse(target, Plant, player))
                 {
-                    OnEndOfTurn.addListener(HealthSelf(1));
+                    //OnEndOfTurn.addListener(HealthSelf(1));
                 }
-            // TODO: Plant Skill damage deals 1 less damage.
-
-            // Bug monster Skill deal 1 more damage, Reptile Normal Attacks deal 1 less damage.
-            case m_trait[Bug] >= 1 && m_trait[Reptile] >= 1:
-            // TODO: 
-
-            // Heal Reptile 1 HP every time that Reptile monster uses a Skill, Plant monster loses 1 HP every turn.
-            case m_trait[Plant] >= 1 && m_trait[Reptile] >= 1:
+                // TODO: Plant Skill damage deals 1 less damage.
+            }
+            else if (entry.Key == MonsterTrait.Bug && m_trait.ContainsKey(MonsterTrait.Reptile))
+            {
+                // Bug monster Skill deal 1 more damage, Reptile Normal Attacks deal 1 less damage.
+            }
+            else if (entry.Key == MonsterTrait.Plant && m_trait.ContainsKey(MonsterTrait.Reptile))
+            {
+                // Heal Reptile 1 HP every time that Reptile monster uses a Skill, Plant monster loses 1 HP every turn.
                 if (MonsterCard.traits == "Reptile" && MonsterNormalAttack.OnUse(target, Plant, player))
                 {
-                    OnEndOfTurn.addListener(HealthSelf(1));
+                    //OnEndOfTurn.addListener(HealthSelf(1));
                 }
-
                 if (MonsterCard.traits == "Plant" && MonsterNormalAttack.OnUse(target, Plant, player))
                 {
-                    OnEndOfTurn.addListener(ApplyDOTSelf(1, 0));
+                    //OnEndOfTurn.addListener(ApplyDOTSelf(1, 0));
                 }
             }
 
         }
+
     }
 }
